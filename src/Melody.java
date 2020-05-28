93655deca7a9b29d8d13af256184344ed80f59db
Tim-17 <tim@holzenkamp.eu>
2020-05-28 14:33:26 +0200

    added Getters and Setters

--- src/Melody.java
+++ src/Melody.java
@@ -6,4 +6,28 @@ public class Melody {
     public Melody(){
 
     }
+
+    public boolean[] getRhythm(){
+        return this.rhythm;
+    }
+
+    public void setRhythm(boolean[] newRhythm){
+        if(newRhythm != null && newRhythm.length > 0){
+            this.rhythm = newRhythm;
+        } else {
+            System.out.println("The rhythm which was inserted does not contain any content!");
+        }
+    }
+
+    public String[] getMelody(){
+        return this.melody;
+    }
+
+    public void setMelody(String[] newMelody){
+        if(newMelody != null && newMelody.length != 0){
+            this.melody = newMelody;
+        } else {
+            System.out.println("The melody which was inserted does not contain any content!");
+        }
+    }
 }
